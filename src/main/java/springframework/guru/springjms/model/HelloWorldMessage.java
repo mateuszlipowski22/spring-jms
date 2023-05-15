48a078ee095b075705ffd932c9bccd3052510ad1
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-05-16 01:59:12 +0200

    adding HelloWorldMessage

---
+++ src/main/java/springframework/guru/springjms/model/HelloWorldMessage.java
@@ -0,0 +1,22 @@
+package springframework.guru.springjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = 1647111697487600475L;
+
+    private UUID id;
+    private String message;
+
+}
